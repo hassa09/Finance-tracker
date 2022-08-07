494b695d69c688dd9a6ad5c18adf6bda126626b1
Your Name~ <you@example.com>
2022-08-07 13:57:45 +0100

    created spending cards that will display the items, date,amout,date,

--- src/components/Finance.js/Finance.js
+++ src/components/Finance.js/Finance.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from "react"
+import Items from "../Items/Items";
+import Cards from "../SpeindingCards/Cards"
 
 function Finance() {
   return (
-    <div>Finance</div>
+    <Cards >
+      <Items
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <Items
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <Items
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      /><Items
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+      <Items
+        title={props.items[4].title}
+        amount={props.items[4].amount}
+        date={props.items[4].date}
+      />
+
+    </Cards>
+
+  
   )
 }
 
